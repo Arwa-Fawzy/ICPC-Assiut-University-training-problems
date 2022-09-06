bd82bec97137e8aab02fc206e5d1382b68377f74
Arwa Fawzy <101527083+Arwa-Fawzy@users.noreply.github.com>
2022-09-06 17:24:43 +0200

    Update Y. The last 2 digits.cpp

--- sheet-1/Y. The last 2 digits.cpp
+++ sheet-1/Y. The last 2 digits.cpp
@@ -1,10 +1,15 @@
 #include <bits/stdc++.h>
-using namespace std;
-
+using namespace std; 
+ 
 int main() {
-    long long a,b,c,d;
+    long long int a,b,c,d;
     cin>>a>>b>>c>>d;
-    long long res=a*b*c*d;
-    cout<<res%100;
+    long long int res=(a%100*b%100*c%100*d%100)%100;
+    if(res<10){
+        cout<<0<<res;
+    }else{
+            cout<<res;
 
+    }
+	
 }
